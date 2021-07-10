66bb59720996d9ce42eed8c51ccd81ec987b3448
MSI\binga <bin.gab03@gmail.com>
2021-07-10 18:03:59 +0800

    push

---
+++ ProjectAlamat/Assets/Scripts/Character_BaseStat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum characterType {player,enemy };
+[CreateAssetMenu]
+public class Character_BaseStat : ScriptableObject
+{
+ public characterType charType;
+    
+    
+
+   
+    public new string name;
+    public Sprite sprite;
+   public int baseHp;
+   public int baseAtk;
+   public int baseDef;
+    // Start is called before the first frame update
+   
+}
--- ProjectAlamat/Assets/Scripts/Salita.cs
+++ ProjectAlamat/Assets/Scripts/Salita.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 [CreateAssetMenu]
 public class Salita : ScriptableObject
 {
+
+ 
     // Start is called before the first frame update
     public string salita;
-
+    
     //in training question
     [Header("In Training Question")]
     public string tagalogSentenceTraining;
