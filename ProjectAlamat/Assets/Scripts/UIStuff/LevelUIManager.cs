23b7c45bbf9b65383decf081c9b398ccda42b816
Renz265 <ramirezrenz45@gmail.com>
2021-07-31 18:09:24 +0800

    Menus and Level Selection scenes done

---
+++ ProjectAlamat/Assets/Scripts/UIStuff/LevelUIManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelUIManager : MonoBehaviour
+{
+    public GameObject emptyPanel;
+    public GameObject PausePanel;
+    public GameObject SettingsPanel;
+
+    [Header("LevelSelectionPanel")]
+    public GameObject LevelSelectionPanel;
+    private float levelSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivatePanel(emptyPanel);
+    }
+
+    public void OnPauseButtonClicked()
+    {
+        PausePanel.SetActive(!PausePanel.activeInHierarchy);
+    }
+
+    public void OnLevelButtonClicked(float levelNumber)
+    {
+        levelSelected = levelNumber;
+        ActivatePanel(LevelSelectionPanel);
+    }
+
+    public void OnYesButtonClicked()
+    {
+        //SceneManager.LoadScene("Level"+levelSelected);
+        SceneManager.LoadScene("trial");
+        Debug.Log("LoadingScene");
+    }
+
+    public void OnMainMenuButtonClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnResumeButtonClicked()
+    {
+        ActivatePanel(emptyPanel);
+    }
+
+    public void OnSettingsButtonClicked()
+    {
+        ActivatePanel(SettingsPanel);
+    }
+
+   public void OnReturnButtonClicked()
+    {
+        ActivatePanel(PausePanel);
+    }
+
+    public void AdjustMusic()
+    {
+        //Adjust Music
+    }
+
+    public void AdjustSound()
+    {
+        //Adjust Sound
+    }
+
+    public void ActivatePanel(GameObject panelToBeActivated)
+    {
+        emptyPanel.SetActive(panelToBeActivated.Equals(emptyPanel));
+        PausePanel.SetActive(panelToBeActivated.Equals(PausePanel));
+        SettingsPanel.SetActive(panelToBeActivated.Equals(SettingsPanel));
+        LevelSelectionPanel.SetActive(panelToBeActivated.Equals(LevelSelectionPanel));
+    }
+}
