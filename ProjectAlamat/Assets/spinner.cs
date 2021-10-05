fec426c95c2e6bad1b149ed4655d2be27aa301a1
MSI\binga <bin.gab03@gmail.com>
2021-10-05 16:41:57 +0800

    sdfasf

--- ProjectAlamat/Assets/spinner.cs
+++ ProjectAlamat/Assets/spinner.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class spinner : MonoBehaviour
+using UnityEngine.EventSystems;
+public class spinner : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     Checker checker;
+    bool isRotating;
+    Vector2 Rot;
+    Vector3 prevPos;
+    Vector3 CurrentPos;
 
    // Vector4 Rot  Quaternion;
     RectTransform rectTransform;
@@ -33,11 +37,118 @@ public class spinner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      
 
     }
- 
 
+    void LateUpdate()
+    {
+        if (isRotating)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (true)
+                {
+
+            
+                    CurrentPos = Input.mousePosition;
+                    float movementDirection =1;
+                   
+                    float A = Vector3.Distance(CurrentPos, prevPos);
+                    //float A = ((CurrentPos.x * CurrentPos.x) - (prevPos.x * prevPos.x)) + ((CurrentPos.y * CurrentPos.y) - (prevPos.y * prevPos.y));
+                    //A = Mathf.Sqrt(A);
+                    float B = Vector3.Distance(CurrentPos, transform.position);
+                    float C = Vector3.Distance(prevPos, transform.position);
+                    //find angle A
+                    float z = (B * B) + (C * C) - (A * A);
+                    z = z / (2 * B * C);
+                    z = Mathf.Acos(z);
+                    z = (z * 180) / Mathf.PI;
+
+
+
+                    if (CurrentPos.x > transform.position.x)
+                    {
+                        //Q1
+                        if (CurrentPos.y > transform.position.y)
+                        {
+                            
+                            if (CurrentPos.y > prevPos.y|| CurrentPos.x < prevPos.x)
+                            {
+                                movementDirection = -1;
+                            }
+                        }
+                        //Q4
+                       else if (CurrentPos.y < transform.position.y)
+                        {
+                            if (CurrentPos.y > prevPos.y || CurrentPos.x > prevPos.x)
+                            {
+                                movementDirection = -1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //Q2
+                        if (CurrentPos.y > transform.position.y)
+                        {
+                            if (CurrentPos.y < prevPos.y || CurrentPos.x < prevPos.x)
+                            {
+                                movementDirection = -1;
+                            }
+                        }
+
+                        //Q4
+                        else if (CurrentPos.y < transform.position.y)
+                        {
+                            if (CurrentPos.y < prevPos.y || CurrentPos.x > prevPos.x)
+                            {
+                                movementDirection = -1;
+                            }
+                        }
+
+
+                    }
+                    Debug.Log(movementDirection);
+
+
+
+
+                    Debug.LogError(z);
+                    //  transform.position = Input.mousePosition;
+                    //transform.rotation = new Vector4(new);
+                    //  transform.position =
+                    //  Input.mousePosition;
+                    transform.eulerAngles += Vector3.forward * -z*movementDirection;
+                    // transform.localRotation = Quaternion.Euler(0,0,z);
+                    //rectTransform.Rotate(new Vector3(0, 0, z));
+                    prevPos = CurrentPos;
+                }
+            }
+
+        }
+
+
+
+       
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isRotating = true;
+
+
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isRotating = false;
+        prevPos = Vector3.zero;
+    }
+
+   
     public void RotateWheel(int x)
     {
         float z= (( (120 * x)));
@@ -53,7 +164,7 @@ public class spinner : MonoBehaviour
     {
 
         Debug.LogWarning((int)rectTransform.eulerAngles.z);
-    checker.WheelChecker(((int)rectTransform.eulerAngles.z %360)==0);
+    checker.WheelChecker(((int)rectTransform.eulerAngles.z %360)<20);
 
 
     }
