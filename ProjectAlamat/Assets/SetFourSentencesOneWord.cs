46403cca89d27c0a777bf976217b077f439fdfa6
bin.gab03@gmail.com <bin.gab03@gmail.com>
2021-10-27 18:34:11 +0800

    adsf

--- ProjectAlamat/Assets/SetFourSentencesOneWord.cs
+++ ProjectAlamat/Assets/SetFourSentencesOneWord.cs
@@ -6,10 +6,9 @@ using UnityEngine.UI;
 public class SetFourSentencesOneWord : MonoBehaviour
 {
 
-<<<<<<< HEAD
+
    [SerializeField] FourPicOneWord_Wheel fourSentencesOneWord_script;
-=======
->>>>>>> b8c1b61e4de600620d48bcea4dcc991ed974810a
+
     [SerializeField] List<Text> Sentences;
     // Start is called before the first frame update
     void Start()
@@ -23,38 +22,29 @@ public class SetFourSentencesOneWord : MonoBehaviour
         
     }
 
-<<<<<<< HEAD
+
     public void SetSenctences(Salita salita, List<string> words)
-=======
-    public void SetSenctences(Salita salita)
->>>>>>> b8c1b61e4de600620d48bcea4dcc991ed974810a
     {
-        Debug.Log("HI");
-        int rand;
-        foreach (Text sentence in Sentences)
+        Debug.LogWarning(words.Count);
+
+        for (int i = 0; i < Sentences.Count; i++)
         {
-<<<<<<< HEAD
+
             Sentences[i].text = salita.Sentences[i];
         }
-        int rand = Random.Range(0, 2);
+      
 
         fourSentencesOneWord_script.setchoices(words);
       
-=======
-            rand = Random.Range(0, 2);
-            if (rand >= 1)
-            {
-                sentence.text = salita.tagalogSentenceTraining;
-            }
-            else
-            {
-                sentence.text = salita.englishSentenceTraining;
-            }
->>>>>>> b8c1b61e4de600620d48bcea4dcc991ed974810a
+
+           
+          
+
            
         }
     
-    }
+    
+
    void SetAnswer()
     { 
     
